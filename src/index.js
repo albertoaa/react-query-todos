7f26a453ff927cc40d0a62ddd9e2a3106af92494
Alberto Aragon Alvarez <ronin8503@gmail.com>
2023-05-30 22:16:21 +0100

    initial setup react query

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { QueryClient, QueryClientProvider, QueryProvider } from 'react-query';
+import { ReactQueryDevTools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevTools initialIsOpen />
+    </QueryClientProvider>
   </React.StrictMode>
 );
